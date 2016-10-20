e4cc15df4065cc358c318f2de512dc3da5f68b70
SebastianHajdus <sebastian.hajdus@gmail.com>
2016-10-20 13:13:41 +0200

    initial verison

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// ==============================
+// Main.js -- JQuery 
+
+// $(document).ready(function(){ 
+
+// 	console.log('DOM READY'); 
+
+// });
+ 
+
+// $(window).load(function() {
+ 
+//   	console.log('HTML , CSS , JS , IMG READY');
+// });
+
+// ==============================
+// Main.js -- Pure Javascript 
+
+ document.addEventListener("DOMContentLoaded", function(event) { // Dom Content Loading
+ 	
+ 	 console.log('DOM READY'); 
+ 
+ });
+
+window.addEventListener('load', function(){
+
+	console.log('HTML , CSS , JS , IMG READY');
+	
+}, false )
