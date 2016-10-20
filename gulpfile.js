d47c8a39adbf82b93f7015eefc905c0aa4bc179f
SebastianHajdus <sebastian.hajdus@gmail.com>
2016-10-20 21:56:02 +0200

    grid

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('scss', function(){ //funkcjonalnosc gulpa; gulp task
         .pipe(sourcemaps.init()) //funkcja gulpa, daje mozliwosc wywolania modulu
         .pipe(sass({
          errLogToConsole: true, //kazdy blad wyswietl na konsoli
-         outputStyle: 'expanded',//okresla jaki wynikowy css, czy ulozony w jednej lini(kompress), czy czytelni
+         outputStyle:'expanded',//okresla jaki wynikowy css, czy ulozony w jednej lini(kompress), czy czytelni
          // sourceComments: true, //skompresowany bedzie sie szybciej wczytywal
        }).on('error', sass.logError))//wysiwetlanie na konsoli
         .pipe(sourcemaps.write())//utworz sourcmape,czyli to co sie wyswietla zakomitowane pod plikiem main.css
