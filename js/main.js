46159ac6b3c2c3f48597e5aa18966226590999a7
SebastianHajdus <sebastian.hajdus@gmail.com>
2016-10-23 15:28:52 +0200

    prototype_3(new font and menu)

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-// ==============================
-// Main.js -- JQuery 
-
-// $(document).ready(function(){ 
-
-// 	console.log('DOM READY'); 
-
-// });
- 
-
-// $(window).load(function() {
- 
-//   	console.log('HTML , CSS , JS , IMG READY');
-// });
-
-// ==============================
-// Main.js -- Pure Javascript 
 
  document.addEventListener("DOMContentLoaded", function(event) { // Dom Content Loading
- 	
- 	 console.log('DOM READY'); 
- 
+
+ 	 console.log('DOM READY');
+
+// --- menu li ---
+
+var nav = document.querySelectorAll('#menu');
+
+
+for(var i = 0; i < nav.length; i++) {
+  nav[i].addEventListener('mouseover', function() {
+    var li = this.querySelector('li');
+
+    if(li !== null) {
+      li.style.display = 'block';
+    }
+  })
+}
+
+for(var i = 0; i <nav.length; i++) {
+  nav[i].addEventListener('mouseout', function () {
+    var li = this.querySelector('li');
+
+    if(li !== null) {
+      li.style.display = "none";
+    }
+  })
+}
+
  });
-
-window.addEventListener('load', function(){
-
-	console.log('HTML , CSS , JS , IMG READY');
-	
-}, false )
